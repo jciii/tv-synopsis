784ea734211e5e9bb81b49f59f715e336980c1bc
James Cardinale <jms.card9.jc@gmail.com>
2020-04-11 19:35:00 -0400

    answer

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const showData = require('./showdata')
+
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
+app.get('/', (request, response) => {
+  return response.render('index', { showData })
+})
+
+app.get('/seasons/:id', (request, response) => {
+  const seasonNum = showData.seasons.find((season) => season.number === parseInt(request.params.id))
+
+  return response.render('seasons', { seasonNum, showData })
+})
+
+
+app.all('*', (request, response) => {
+  return response.sendStatus(404)
+})
+
+app.listen(7142, () => {
+  // eslint-disable-next-line no-console
+  console.log('listening on 7142')
+})
+
